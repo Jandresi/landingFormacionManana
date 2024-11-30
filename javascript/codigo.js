437dafab456f8bcf33251463347576dbdf3ec93d
JANDRESI-PC\jandr <jandresi@hotmail.com>
2024-11-29 21:51:05 -0500

    Se maquetan todos los espacios de formacion

--- javascript/codigo.js
+++ javascript/codigo.js
@@ -1,3 +1,7 @@
 function submitContacto(event) {
     event.preventDefault();
+
+    const {nombreTutor, email, celular, curso} = event.target;
+
+    console.log({nombreTutor, email, celular, curso});
 }
